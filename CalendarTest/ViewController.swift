5b8b4c0a26aa425a1ff98c75c3ec10eba72cf139
Craig Hewlett <craig.hewlett@me.com>
2021-05-31 12:08:45 -0600

    Initial

--- CalendarTest/ViewController.swift
+++ CalendarTest/ViewController.swift
@@ -6,14 +6,108 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
-class ViewController: UIViewController {
+class DateCell: JTAppleCell {
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet weak var selectedView: UIImageView!
+    @IBOutlet weak var indicator_workLog: UIImageView!
+    @IBOutlet weak var indicator_repairRequest: UIImageView!
+    @IBOutlet weak var indicator_maintenance: UIImageView!
+}
+
+class DateHeader: JTAppleCollectionReusableView  {
+    @IBOutlet var monthTitle: UILabel!
+    
+}
+
+//Calendar Code by patchthecode/JTAppleCalendar
+//https://github.com/patchthecode/JTAppleCalendar
+
+class ViewController: UIViewController, JTAppleCalendarViewDataSource, JTAppleCalendarViewDelegate {
+    
+    @IBOutlet weak var calendar: JTAppleCalendarView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        calendar.ibCalendarDelegate = self
+        calendar.ibCalendarDataSource = self
+        
+        calendar.scrollingMode = .stopAtEachCalendarFrame
+        calendar.scrollDirection = .horizontal
+        
+    }
+    
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+        let startDate = formatter.date(from: "2020 12 01")!
+        let endDate = Date()
+        return ConfigurationParameters(startDate: startDate,
+                                       endDate: endDate,
+                                       generateInDates: .forAllMonths,
+                                       generateOutDates: .tillEndOfGrid)
+    }
+    
+    func configureCell(view: JTAppleCell?, cellState: CellState) {
+        guard let cell = view as? DateCell  else { return }
+        cell.dateLabel.text = cellState.text
+        handleCellTextColor(cell: cell, cellState: cellState)
+        handleCellSelected(cell: cell, cellState: cellState)
+    }
+    
+    func handleCellTextColor(cell: DateCell, cellState: CellState) {
+       if cellState.dateBelongsTo == .thisMonth {
+          cell.dateLabel.textColor = UIColor.black
+       } else {
+          cell.dateLabel.textColor = UIColor.gray
+       }
+    }
+    
+    func handleCellSelected(cell: DateCell, cellState: CellState) {
+        if cellState.isSelected {
+            cell.selectedView.layer.cornerRadius =  8
+            cell.selectedView.isHidden = false
+        } else {
+            cell.selectedView.isHidden = true
+        }
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "dateCell", for: indexPath) as! DateCell
+        self.calendar(calendar, willDisplay: cell, forItemAt: date, cellState: cellState, indexPath: indexPath)
+        return cell
+    }
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        configureCell(view: cell, cellState: cellState)
+
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        configureCell(view: cell, cellState: cellState)
     }
 
+    func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        configureCell(view: cell, cellState: cellState)
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, shouldSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) -> Bool {
+        return true // Based on a criteria, return true or false
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, headerViewForDateRange range: (start: Date, end: Date), at indexPath: IndexPath) -> JTAppleCollectionReusableView {
+        let formatter = DateFormatter()  // Declare this outside, to avoid instancing this heavy class multiple times.
+        formatter.dateFormat = "MMMM YYYY"
+        
+        let header = calendar.dequeueReusableJTAppleSupplementaryView(withReuseIdentifier: "DateHeader", for: indexPath) as! DateHeader
+        header.monthTitle.text = formatter.string(from: range.start)
+        return header
+    }
 
+    func calendarSizeForMonths(_ calendar: JTAppleCalendarView?) -> MonthSize? {
+        return MonthSize(defaultSize: 50)
+    }
+    
 }
 
